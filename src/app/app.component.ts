029d36be84f049de2da1d95a00e02d4bb5fd5d26
Rohan Jacob <rohani@email.unc.edu>
2024-11-21 14:11:34 -0500

    added light and dark mode button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'comp426FinalProject';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    const defaultTheme = 'light-mode';
+    this.renderer.addClass(document.body, defaultTheme);
+  }
+
+  toggleTheme() {
+    const currentTheme = document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
+    const newTheme = currentTheme === 'dark-mode' ? 'light-mode' : 'dark-mode';
+    document.body.classList.remove(currentTheme);
+    document.body.classList.add(newTheme);
+  }
 }
+
