c37f3cee281b2ad02f1b795de9838bce03db723e
Rohan Jacob <rohani@email.unc.edu>
2024-12-02 22:55:22 -0500

    add db to gitignore

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { DiaryItem } from './diaryItem.mjs';
+import { User } from './user.mjs';
 
 const app = express();
 const port = 3000;
@@ -38,6 +39,17 @@ app.delete('/diary/:id', async (req, res) => {
     return;
   } 
   res.json(true);
+})
+
+app.post('/user', async (req, res) => {
+  let user = await User.create(req.body);
+
+  if (!user) {
+    res.status(400).send("Bad request");
+    return;
+  }
+
+  res.status(201).json(user.json());
 })
 
 app.listen(port, () => {
