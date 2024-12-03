f685ca53cf549a1e1115bdeebd68d4ef60c04a48
Rohan Jacob <rohani@email.unc.edu>
2024-12-02 22:44:19 -0500

    added some user backend func and estimatation of api calls from the frontend to backend

--- backend/user.mjs
+++ backend/user.mjs
@@ -1,3 +1,5 @@
+import { db } from "./db.mjs";
+
 export class User {
 
     #id
@@ -10,18 +12,43 @@ export class User {
         this.#name = name;
     }
 
-    static create(data) {
-        // validate parameter, 
-        // call constructor, 
-        // add user to all users
-        // return user
+    static async create(data) {
+        if ((data !== undefined) && (data instanceof Object)
+        && (data.name !== undefined) && (typeof data.name == 'string')) {
+            try {
+                let db_result = await db.run(
+                    'INSERT INTO users (name) VALUES (?)', 
+                    data.name
+                );
+                let user = new User(db_result.lastId, data.name);
+            return user;
+            } catch (e) {
+                console.log(e);
+                return [];
+            }
+        }
     }
 
-    static getAllIds() {
-
+    static async getAll() {
+        //return from the database all the users.
+        try {
+            let rows = await db.all('SELECT * from users');
+            return rows;
+        } catch (e) {
+            console.log(e);
+            return [];
+        }
     }
 
-    static findByID(id) {
+    static async findByID(id) {
+        //return from the database the user with the given id.
+        try {
+            let row = await db.get('SELECT * from users WHERE id = ?', id);
+            return row;
+        } catch (e) {
+            console.log(e);
+            return [];
+        }
 
     }
 
